e62a02f00f628110579f681401ca1529796ca010
gobjjj <rhqudwls13@naver.com>
2024-09-18 17:45:14 +0900

    플레이어 생성

---
+++ src/oop/MusicPlayerMain.java
@@ -0,0 +1,15 @@
+package oop;
+
+public class MusicPlayerMain {
+
+    public static void main(String[] args) {
+
+        MusicPlayer musicplayer1 = new MusicPlayer();
+        musicplayer1.on();
+        musicplayer1.volumeUp();
+        musicplayer1.volumeDown();
+        musicplayer1.off();
+
+    }
+
+}
