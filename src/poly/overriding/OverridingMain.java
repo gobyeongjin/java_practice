996773a2aff55b03f920b3f13b17962e120d6e27
gobjjj <rhqudwls13@naver.com>
2024-10-01 15:32:31 +0900

    다형성2, instanc of : 다운캐스팅 수행하기전에 intance of를 사용해서 원하는 타입으로 변경이 가능한지 확인후 수행

---
+++ src/poly/overriding/OverridingMain.java
@@ -0,0 +1,24 @@
+package poly.overriding;
+
+import javax.print.DocFlavor.STRING;
+
+public class OverridingMain {
+    public static void main(String[] args) {
+        Child child = new Child();
+        System.out.println("Child->Child");
+        System.out.println("value = " + child.value);
+        child.method();
+
+        Parent parent = new Parent();
+        System.out.println("Parent->Parent");
+        System.out.println("value = " + parent.value);
+        parent.method();
+
+        Parent poly = new Child();
+        System.out.println("Parent -> child");
+        System.out.println("value = " + poly.value);
+        poly.method();
+
+    }
+
+}
